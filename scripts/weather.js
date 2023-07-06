33c1979f288c6c08000450ebd8c9da7cb82345eb
Camwp <camwpedro@gmail.com>
2023-07-05 18:40:57 -0700

    weather

---
+++ scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=7c71213c74f3ed66fde3c04f0ecec19c";
+
+async function apiFetch(url) {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+      } else {
+        throw new Error('API request failed');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  }
+apiFetch(url);
