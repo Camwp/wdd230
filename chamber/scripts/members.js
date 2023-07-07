7e5dce1d3c4d3168db451ac43e63cd36aea4d3aa
Cameron Pedro <camwcats@gmail.com>
2023-07-06 22:27:57 -0700

    directory

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,57 +1,82 @@
-// Sample JSON data
-const companies = "https://camwp.github.io/wdd230/chamber/members.json";
-  
-  // Function to create a company card
-  function createCompanyCard(company) {
-    const card = document.createElement("div");
-    card.classList.add("company-card");
-  
-    const name = document.createElement("div");
-    name.classList.add("company-name");
+const url = 'https://camwp.github.io/wdd230/chamber/members.json';
+const cards = document.querySelector('#company-cards');
+const viewTypeSelect = document.querySelector('#view-type');
+
+let currentViewType = 'gridC';
+
+async function getCompanyData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch company data');
+    }
+    const data = await response.json();
+    displayCompanies(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getCompanyData(url);
+
+const displayCompanies = (companies) => {
+  companies.forEach((company) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement('section');
+    let name = document.createElement('h2');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('a');
+    let image = document.createElement('img');
+    let membership = document.createElement('p');
+
+    // Build the h2 element to display the company name
     name.textContent = company.name;
-  
-    const address = document.createElement("div");
-    address.classList.add("company-address");
-    address.textContent = company.address;
-  
-    const phone = document.createElement("div");
-    phone.classList.add("company-phone");
-    phone.textContent = company.phone;
-  
-    const website = document.createElement("a");
-    website.classList.add("company-website");
-    website.textContent = company.website;
-    website.href = company.website;
-  
-    const image = document.createElement("img");
-    image.classList.add("company-image");
-    image.src = company.image;
-    image.alt = company.name + " logo";
-  
-    const membership = document.createElement("div");
-    membership.classList.add("company-membership");
-    membership.textContent = "Membership Level: " + company.membership_level;
-  
+
+    // Build the address element
+    address.textContent = `Address: ${company.address}`;
+
+    // Build the phone element
+    phone.textContent = `Phone: ${company.phone}`;
+
+    // Build the website link element
+    website.textContent = "Website";
+    website.setAttribute('href', company.website);
+
+    // Build the image element
+    image.setAttribute('src', company.image);
+    image.setAttribute('alt', `Logo of ${company.name}`);
+    image.setAttribute('loading', 'lazy');
+    image.setAttribute('width', '200');
+    image.setAttribute('height', '200');
+
+    // Build the membership element
+    membership.textContent = `Membership Level: ${company.membership_level}`;
+
+    // Append the card section with the created elements
     card.appendChild(name);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(website);
     card.appendChild(image);
     card.appendChild(membership);
+
+    cards.appendChild(card);
+  });
+}
+
+function updateViewType(viewType) {
+    currentViewType = viewType;
   
-    return card;
-  }
+    const companyCards = document.querySelectorAll('#company-cards');
   
-  // Function to render company cards
-  function renderCompanyCards() {
-    const companyCardsContainer = document.getElementById("company-cards");
-  
-    companies.forEach(function(company) {
-      const companyCard = createCompanyCard(company);
-      companyCardsContainer.appendChild(companyCard);
+    companyCards.forEach((card) => {
+      card.classList.remove('list', 'gridC');
+      card.classList.add(currentViewType);
     });
   }
   
-  // Call the function to render company cards
-  renderCompanyCards();
-  
+  viewTypeSelect.addEventListener('change', (event) => {
+    const viewType = event.target.value;
+    updateViewType(viewType);
+  });
